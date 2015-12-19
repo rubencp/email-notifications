22a8b8edc0cc10c6f16b34d21351443535ea1bdc
Diana Garcia <dianitacgp@imac.speedport_w_724v_09011603_00_018>
2015-12-19 14:39:07 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+ruby "2.1.5"
+
+gem 'shopify-sinatra-app', '~> 0.2.0', branch: 'master'
+gem 'sinatra-activerecord'
+gem 'sinatra-twitter-bootstrap', require: 'sinatra/twitter-bootstrap'
+gem 'rack-flash3', require: 'rack-flash'
+
+group :production do
+  gem 'pg'
+end
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'byebug'
+end
+
+group :development do
+  gem 'rake'
+  gem 'foreman'
+  gem 'dotenv'
+end
+
+group :test do
+  gem 'mocha', require: false
+  gem 'minitest'
+  gem 'rack-test'
+  gem 'fakeweb'
+end
---
+++ lib/app.rb
@@ -0,0 +1,53 @@
+require 'sinatra/shopify-sinatra-app'
+
+class SinatraApp < Sinatra::Base
+  register Sinatra::Shopify
+
+  # set the scope that your app needs, read more here:
+  # http://docs.shopify.com/api/tutorials/oauth
+  set :scope, 'read_products, read_orders'
+
+  # Your App's Home page
+  # this is a simple example that fetches some products
+  # from Shopify and displays them inside your app
+  get '/' do
+    shopify_session do
+      @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+      erb :home
+    end
+  end
+
+  # this endpoint recieves the uninstall webhook
+  # and cleans up data, add to this endpoint as your app
+  # stores more data.
+  post '/uninstall' do
+    webhook_session do |params|
+      current_shop.destroy
+    end
+  end
+
+  private
+
+  # This method gets called when your app is installed.
+  # setup any webhooks or services you need on Shopify
+  # inside here.
+  def after_shopify_auth
+    # shopify_session do
+      # create an uninstall webhook, this webhook gets sent
+      # when your app is uninstalled from a shop. It is good
+      # practice to clean up any data from a shop when they
+      # uninstall your app:
+
+      # uninstall_webhook = ShopifyAPI::Webhook.new(
+      #   topic: 'app/uninstalled',
+      #   address: "#{base_url}/uninstall",
+      #   format: 'json'
+      # )
+      # begin
+      #   uninstall_webhook.save!
+      # rescue => e
+      #   raise unless uninstall_webhook.persisted?
+      # end
+    # end
+  end
+end
